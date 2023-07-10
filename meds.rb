abbe4e2660ed8879e4d8c987799fc5e50cdafb60
Donald Lee <kndonlee@gmail.com>
2023-07-09 19:40:23 -0600

    v3.2.9 add baclofen back to the rotation

--- meds.rb
+++ meds.rb
@@ -61,7 +61,7 @@ class MedDash
 
   attr_accessor :meds
   def initialize
-    @version = "3.2.8"
+    @version = "3.2.9"
     @hostname = `hostname`.strip
     reset_meds
 
@@ -147,7 +147,7 @@ class MedDash
     @meds = {}
     @meds[:morphine]       = Med.new(name: :morphine,       interval:8,  required:12, default_dose:15,   half_life:3.5*3600,   max_dose:0,     dose_units: :mg,   display:true,  display_log:true,  emoji:"1F480")
     @meds[:morphine_bt]    = Med.new(name: :morphine_bt,    interval:8,  required:48, default_dose:7.5,  half_life:3*3600,     max_dose:0,     dose_units: :mg,   display:false, display_log:false, emoji:"1F48A")
-    @meds[:baclofen]       = Med.new(name: :baclofen,       interval:8,  required:48, default_dose:7.5,  half_life:4*3600,     max_dose:0,     dose_units: :mg,   display:false, display_log:false, emoji:"26A1")
+    @meds[:baclofen]       = Med.new(name: :baclofen,       interval:6,  required:12, default_dose:7.5,  half_life:4*3600,     max_dose:0,     dose_units: :mg,   display:true,  display_log:false, emoji:"26A1")
     @meds[:robaxin]        = Med.new(name: :robaxin,        interval:3,  required:48, default_dose:500,  half_life:1.1*3600,   max_dose:0,     dose_units: :mg,   display:true,  display_log:true,  emoji:"26A1")
     @meds[:lyrica]         = Med.new(name: :lyrica,         interval:12, required:12, default_dose:150,  half_life:6.3*3600,   max_dose:0,     dose_units: :mg,   display:true,  display_log:true,  emoji:"1F9E0")
 
