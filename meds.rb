1613ce342ee30c26db7730daceaedd27de894ffc
Donald Lee <kndonlee@gmail.com>
2024-08-20 23:15:26 -0600

    v4.6.2 add herbs

--- meds.rb
+++ meds.rb
@@ -64,7 +64,7 @@ class MedDash
 
   attr_accessor :meds
   def initialize
-    @version = "4.6.1"
+    @version = "4.6.2"
     @hostname = `hostname`.strip
     reset_meds
 
@@ -251,6 +251,7 @@ class MedDash
     @meds[:c60]            = Med.new(name: :c60,            interval:24,   required:48, default_dose:10,   half_life:3600,      max_dose:0,     dose_units: :unit, display: :no,        display_log:false, announce:false,  emoji:"1F4A6")
     @meds[:coco]           = Med.new(name: :coco,           interval:24,   required:48, default_dose:10,   half_life:3600,      max_dose:0,     dose_units: :mg,   display: :no,        display_log:false, announce:false,  emoji:"1F4A6")
     @meds[:ubiquinol]      = Med.new(name: :ubiquinol,      interval:4,    required:6,  default_dose:50,   half_life:3600*33,   max_dose:0,     dose_units: :mg,   display: :yes,       display_log:false, announce:false,  emoji:"1F4A6")
+    @meds[:herbs]          = Med.new(name: :herbs,          interval:4,    required:24, default_dose:1,    half_life:3600,      max_dose:0,     dose_units: :unit, display: :yes,       display_log:false, announce:false,  emoji:"1F4A6")
     @meds[:ergothioneine]  = Med.new(name: :ergothioneine,  interval:4,    required:6,  default_dose:25,   half_life:3600,      max_dose:0,     dose_units: :mg,   display: :no,        display_log:false, announce:false,  emoji:"1F4A6")
     @meds[:dummy]          = Med.new(name: :dummy,          interval:4,    required:6,  default_dose:25,   half_life:3600,      max_dose:0,     dose_units: :mg,   display: :no,        display_log:false, announce:false,  emoji:"1F4A6")
 
