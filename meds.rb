2cdcae1e2f7360afba44e8587733840efe7ecba1
Donald Lee <kndonlee@gmail.com>
2024-09-14 18:23:54 -0600

    v4.6.8 HD to 1g

--- meds.rb
+++ meds.rb
@@ -64,7 +64,7 @@ class MedDash
 
   attr_accessor :meds
   def initialize
-    @version = "4.6.7"
+    @version = "4.6.8"
     @hostname = `hostname`.strip
     reset_meds
 
@@ -238,7 +238,7 @@ class MedDash
     @meds[:calcium_aep]    = Med.new(name: :calcium_aep,    interval:4,    required:48, default_dose:1850, half_life:3600,      max_dose:0,     dose_units: :mg,   display: :no,        display_log:false, announce:false,  emoji:"1F4AE")
     @meds[:fem]            = Med.new(name: :fem,            interval:24,   required:24, default_dose:1,    half_life:3600,      max_dose:0,     dose_units: :unit, display: :no,        display_log:false, announce:false,  emoji:"1F4A6")
     @meds[:roe]            = Med.new(name: :roe,            interval:24,   required:24, default_dose:28,   half_life:3600,      max_dose:0,     dose_units: :g,    display: :no,        display_log:false, announce:false,  emoji:"1F4A6")
-    @meds[:hd]             = Med.new(name: :hd,             interval:24,   required:24, default_dose:1,    half_life:3600,      max_dose:3,     dose_units: :g,    display: :yes,       display_log:false, announce:false,  emoji:"1F4A6")
+    @meds[:hd]             = Med.new(name: :hd,             interval:24,   required:24, default_dose:1,    half_life:3600,      max_dose:1,     dose_units: :g,    display: :yes,       display_log:false, announce:false,  emoji:"1F4A6")
     @meds[:phys_thr]       = Med.new(name: :phys_thr,       interval:24,   required:48, default_dose:1,    half_life:3600,      max_dose:0,     dose_units: :unit, display: :no,        display_log:false, announce:false,  emoji:"1F4A6")
     @meds[:colostrum]      = Med.new(name: :colostrum,      interval:23,   required:48, default_dose:500,  half_life:3600,      max_dose:0,     dose_units: :mg,   display: :no,        display_log:false, announce:false,  emoji:"1F4A6")
     @meds[:quercetin]      = Med.new(name: :quercetin,      interval:23,   required:48, default_dose:500,  half_life:3600,      max_dose:0,     dose_units: :mg,   display: :no,        display_log:false, announce:false,  emoji:"1F4A6")
