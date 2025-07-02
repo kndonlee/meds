ea95ce42b0ab346a2f68358567992b96549efb01
Donald Lee <kndonlee@gmail.com>
2025-07-01 20:34:22 -0600

    v4.7.0 show choline/pc/choline in different sections

--- meds.rb
+++ meds.rb
@@ -64,7 +64,7 @@ class MedDash
 
   attr_accessor :meds
   def initialize
-    @version = "4.6.27"
+    @version = "4.7.0"
     @hostname = `hostname`.strip
     reset_meds
 
@@ -188,14 +188,10 @@ class MedDash
     @meds[:oxycodone]      = Med.new(name: :oxycodone,      interval:4,    required:5,  default_dose:5,    half_life:3*3600,     max_dose:0,     dose_units: :mg,   display: :on_dose,  display_log:true,  announce:false,  emoji:"1F48A")
     @meds[:hydrocodone]    = Med.new(name: :hydrocodone,    interval:4,    required:48, default_dose:10,   half_life:3.8*3600,   max_dose:0,     dose_units: :mg,   display: :on_dose,  display_log:true,  announce:false,  emoji:"1F48A")
     @meds[:dilauded]       = Med.new(name: :dilauded,       interval:4,    required:6,  default_dose:1,    half_life:2.5*3600,   max_dose:0,     dose_units: :mg,   display: :on_dose,  display_log:true,  announce:false,  emoji:"1F48A")
-    @meds[:baclofen]       = Med.new(name: :baclofen,       interval:23.9, required:48, default_dose:5,    half_life:4*3600,     max_dose:0,     dose_units: :mg,   display: :yes,      display_log:false, announce:false,  emoji:"26A1")
+    @meds[:baclofen]       = Med.new(name: :baclofen,       interval:23.9, required:48, default_dose:5,    half_life:4*3600,     max_dose:0,     dose_units: :mg,   display: :yes,      display_log:false, announce:false,  emoji:"1F535")
     @meds[:robaxin]        = Med.new(name: :robaxin,        interval:2,    required:10, default_dose:500,  half_life:1.1*3600,   max_dose:0,     dose_units: :mg,   display: :on_dose,  display_log:true,  announce:false,  emoji:"26A1")
     @meds[:lyrica]         = Med.new(name: :lyrica,         interval:10,   required:18, default_dose:18,   half_life:6.3*3600,   max_dose:0,     dose_units: :mg,   display: :no,       display_log:false, announce:false,  emoji:"1F9E0")
     @meds[:periactin]      = Med.new(name: :periactin,      interval:6,    required:8,  default_dose:2,    half_life:7.5*3600,   max_dose:12,    dose_units: :mg,   display: :no,       display_log:false, announce:false,  emoji:"1F971")
-    @meds[:choline]        = Med.new(name: :choline,        interval:6.5,  required:6.5,default_dose:6.25, half_life:10*3600,    max_dose:0,     dose_units: :mg,   display: :on_dose,  display_log:true,  announce:false,  emoji:"1F971")
-    @meds[:phosphatidyl_c] = Med.new(name: :phosphatidyl_c, interval:4,    required:48, default_dose:420,  half_life:3600,       max_dose:0,     dose_units: :mg,   display: :no,       display_log:false, announce:false,  emoji:"1F9E0")
-    @meds[:cdp]            = Med.new(name: :cdp,            interval:4,    required:48, default_dose:420,  half_life:3600,       max_dose:0,     dose_units: :mg,   display: :no,       display_log:false, announce:false,  emoji:"1F9E0")
-    #    @meds[:choline_b] = Med.new(name: :choline_bitartrate,  interval:6,    required:8,  default_dose:6.25, half_life:10*3600,    max_dose:0,     dose_units: :mg,   display: :yes,      display_log:false, announce:false,  emoji:"1F971")
 
     @meds[:esgic]          = Med.new(name: :esgic,          interval:4,    required:48, default_dose:1,    half_life:35*3600,    max_dose:0,     dose_units: :unit, display: :yes,      display_log:true,  announce:false,  emoji:"1F915")
     @meds[:tylenol]        = Med.new(name: :tylenol,        interval:4,    required:96, default_dose:500,  half_life:3*3600,     max_dose:0,     dose_units: :mg,   display: :yes,      display_log:true,  announce:false,  emoji:"1F915")
@@ -205,6 +201,11 @@ class MedDash
     @meds[:ondansetron]    = Med.new(name: :ondansetron,    interval:4,    required:48, default_dose:4,    half_life:4*3600,     max_dose:0,     dose_units: :mg,   display: :no,       display_log:false, announce:false,  emoji:"1F48A")
     @meds[:soma]           = Med.new(name: :soma,           interval:4,    required:48, default_dose:350,  half_life:2*3600,     max_dose:0,     dose_units: :mg,   display: :yes,      display_log:false, announce:false,  emoji:"1F48A")
     @meds[:lansoprazole]   = Med.new(name: :lansoprazole,   interval:24,   required:24, default_dose:15,   half_life:1.7*3600,   max_dose:15,    dose_units: :mg,   display: :no,       display_log:false, announce:false,  emoji:"1F48A")
+
+    @meds[:cdp]            = Med.new(name: :cdp,            interval:4,    required:48, default_dose:420,  half_life:3600,       max_dose:0,     dose_units: :mg,   display: :yes,       display_log:false, announce:false,  emoji:"26A1")
+    @meds[:phosphatidyl_c] = Med.new(name: :phosphatidyl_c, interval:4,    required:48, default_dose:420,  half_life:3600,       max_dose:0,     dose_units: :mg,   display: :yes,       display_log:false, announce:false,  emoji:"1F9E0")
+    @meds[:choline_b]      = Med.new(name: :choline_b,      interval:6.5,  required:6.5,default_dose:6.25, half_life:10*3600,    max_dose:0,     dose_units: :mg,   display: :yes,       display_log:true,  announce:false,  emoji:"1F971")
+
     @meds[:taurine]        = Med.new(name: :taurine,        interval:4,    required:6,  default_dose:500,  half_life:3600,       max_dose:6500,  dose_units: :mg,   display: :yes,      display_log:true,  announce:false,  emoji:"1F431")
     @meds[:calcium]        = Med.new(name: :calcium,        interval:4,    required:6,  default_dose:250,  half_life:2*3600,     max_dose:1750,  dose_units: :mg,   display: :yes,      display_log:true,  announce:false,  emoji:"1F9B4")
     @meds[:iron]           = Med.new(name: :iron,           interval:3,    required:4,  default_dose:10.5, half_life:5*3600,     max_dose:31.5,  dose_units: :mg,   display: :no,       display_log:true,  announce:false,  emoji:"1FA78")
@@ -278,6 +279,8 @@ class MedDash
     @meds[:calcium_aep].add_match_term("calcium aep")
     @meds[:phys_thr].add_match_term("physical")
     @meds[:phys_thr].add_match_term("physical therapy")
+    @meds[:choline_b].add_match_term("choline")
+
   end
 
   def hide_narcotics(epoch)
@@ -717,9 +720,9 @@ class MedDash
       end
       next unless show?(med.to_s)
 
-      if med == :taurine
+      if med == :cdp
         s += "#{line(color:240)}\n"
-      elsif  med == :msm || med == :esgic || med == :azelastine
+      elsif  med == :msm || med == :esgic || med == :azelastine || med == :taurine
         s += "#{ANSI.clear_line}\n"
       end
 
